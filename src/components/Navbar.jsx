7b6e678d3f85f4b24903451a479e373a3cd964c9
MatheusDourado <lokabrel.dourado@gmail.com>
2022-09-19 21:32:10 -0300

    Criado layout final do component Navbar HTML e CSS finalizados

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,42 @@ import { FaSearch } from "react-icons/fa"
 const Navbar = () => {
     return (
         <nav className="navbar-custom">
-            <Link className="navbar-custom-logo" to="/" title="TESTE">M.D. TMDB</Link>
+            <Link className="navbar-custom-logo" to="/" >
+                <img src="src/assets/img/logo.svg" alt="Logo" />
+            </Link>
             <ul>
                 <li>
                     <Link className="nav-link-custom" aria-current="page" to="/">Home</Link>
                 </li>
-                <li>
+                <li className="movies">
                     <Link className="nav-link-custom" to="/movie/:id">Filmes</Link>
+
+                    <div className="dropdown-movies">
+                        <ul>
+                            <li>Populares</li>
+                            <li>Em cartaz</li>
+                            <li>Próximas Estreias</li>
+                            <li>Mais bem avaliados</li>
+                        </ul>
+                    </div>
                 </li>
-                <li>
+
+                <li className="series">
                     <Link className="nav-link-custom" to="/search">Séries</Link>
+
+                    <div className="dropdown-series">
+                        <ul>
+                            <li>Populares</li>
+                            <li>Em exibição hoje</li>
+                            <li>Na TV</li>
+                            <li>Mais bem avaliados</li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
             <div className="nav-search">
                 <input type="search" placeholder="Buscar" aria-label="Buscar"/>
-                <button type="submit"><FaSearch/></button>
+                <button className="nav-submit" type="submit"><FaSearch/></button>
             </div>
         </nav>
     )
